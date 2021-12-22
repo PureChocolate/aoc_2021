307d7c3fecdb70decb2ef4626a83d4252175b68b
Pure Chocolate <15825036+PureChocolate@users.noreply.github.com>
2021-12-22 17:20:12 -0500

    parsed input as actual lists

---
+++ day18/day18.py
@@ -0,0 +1,56 @@
+data = []
+data.append("[[[[4,3],4],4],[7,[[8,4],9]]]")
+data.append("[1,1]")
+
+def printer(num,i):
+    for a in range(len(num)):
+        if i == a:
+            print("(" + num[a] + ")",end="")
+        else: print(num[a], end="")
+    print("")
+        
+
+def makeLists(num,i):
+    #print("Num: ", num, i, len(num))
+    #printer(num,i)
+    li = []
+    while i < len(num):
+        if num[i] == "[":
+            u = makeLists(num,i+1)
+            li.append(u[0])
+            #print("I BEFORE:",i)
+            i = u[1]
+            #print("I AFTER:", i)
+        elif num[i] == "]": break
+        elif num[i] == ",":
+            i += 1
+            continue
+        else:
+            #print("Hit num: ", num[i])
+            li.append(int(num[i]))
+        i += 1
+    #print("RETURNED:",li, i)      
+    return (li,i)
+
+def addnum(num,num2):
+    li = []
+    li.append(num)
+    li.append(num2)
+    return li
+
+def parse(data):
+    for a in range(len(data)):
+        data[a] = makeLists(data[a],1)[0]
+        print(data[a])
+
+def runNum(data,start,a):
+    for a in range(1, len(data)):
+        cur = addnum(start,data[a])
+        print(cur)
+    
+
+
+parse(data)
+runNum(data,data[0],1)
+#print(addnum(start,data[0]))
+    
